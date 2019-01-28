9a067dd534e35173653f25a19d101033081c105b
M692 <M692@users.noreply.github.com>
2019-01-29 06:37:09 +0800

    Update boot.py

--- episode1/boot.py
+++ episode1/boot.py
@@ -1,1 +1,19 @@
+import discord
+from discord.ext import commands
 
+TOKEN = "" #Add your token here
+
+client = commands.Bot(command_prefix = "!")
+client.remove_command('help')
+
+@client.event
+async def on_ready():
+    print("Bot is online")
+
+
+@client.command(pass_context=True)
+async def ping(ctx):
+    await client.say("Pong!")
+
+
+client.run(TOKEN)
